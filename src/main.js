2e2f5e765c24bf612b640ae306516fea4c6ba3eb
刘嘉霖 <2243886124@qq.com>
2022-07-20 21:26:27 +0800

    feat(新功能)：content封装

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './style/icon/iconfont.css'
+
+import './styles/icon/iconfont.css'
 
 import request from '@/utils/request'
-Vue.config.productionTip = false
-Vue.prototype.$axios = request
 Vue.use(ElementUI)
+
+Vue.prototype.$axios = request
+
+Vue.config.productionTip = false
+
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: () => import('../views/Home')
-  // },
   {
     path: '/',
-    name: 'table',
-    component: () => import('../views/table')
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/advert',
+    name: 'advert',
+    component: () => import('../views/Advert.vue')
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
